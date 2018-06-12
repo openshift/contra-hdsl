08fc890cd7e72c1ba87e37b54d0e53109ed1a106
Rob Nester <rnester@redhat.com>
2018-06-12 14:00:31 -0400

    ADD baseDir parameter to methods using playbooks
    Updated configureInfra and executeTests to use an optional paramater,
    ```baseDir``` which would specify a directory, relative to the
    workspace, in the form of 'relative/path' that would be prepended to the
    playbook path specified in the contra.yml file.
    
    This feature would allow a respository to be checked out to a given
    subdirectory in the workspace, while alleviating the need to have said
    subdirectory present in the contra.yml file. In this scenario, the paths
    in the contra.yml would still be relative to the root of the repository
    in which the playbooks are contained.

--- vars/executeTests.groovy
+++ vars/executeTests.groovy
@@ -5,6 +5,8 @@ import org.centos.contra.Infra.Utils
  * yaml file.
  * @param config: An optional map that holds configuration parameters.
  * @param config.verbose: A key with a Boolean value which enables verbose output from the ansible-playbook execution.
+ * @param config.baseDir: A key with a String value which indicates a baseDir relative to the workspace which should be
+ *                        prepended to the playbook location
  * @return
  */
 def call(Map<String, String> config = [:]) {
@@ -30,7 +32,7 @@ def call(Map<String, String> config = [:]) {
     // Let's execute our playbooks!
     if ( configData.tests.playbooks ) {
         configData.tests.playbooks.each { LinkedHashMap playbook ->
-            String playbook_path = playbook.location
+            String playbook_path = config.baseDir ? "${config.baseDir}/${playbook.location}" : playbook.location
             String paramString = "-e ${playbook.vars.iterator().join(', -e ')}"
             infraUtils.executeInAnsible(playbook_path, paramString, config.verbose as Boolean)
         }
